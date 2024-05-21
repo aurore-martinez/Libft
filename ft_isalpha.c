49866708092452c18a6fc73480fcd76301c055b8
Aurore Martinez <aumartin@bess-f1r3s6.clusters.42paris.fr>
2024-05-21 09:54:09 +0200

    isalpha compile et check ok

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:55:07 by aumartin          #+#    #+#             */
-/*   Updated: 2024/05/20 13:53:13 by aumartin         ###   ########.fr       */
+/*   Updated: 2024/05/21 09:53:25 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,3 @@ int	ft_isalpha(int c)
 		return (1);
 	return (0);
 }
-
-/* #include <stdio.h>
-#include <ctype.h>
-int main()
-{
-	printf("%d : %d\n", ft_isalpha('a'), isalpha('a'));
-	printf("%d : %d\n", ft_isalpha('\0'), isalpha('\0'));
-	printf("%d : %d\n", ft_isalpha('1'), isalpha('1'));
-	printf("%d : %d\n", ft_isalpha('N'), isalpha('N'));
-	printf("%d : %d\n", ft_isalpha('\n'), isalpha('\n'));
-	printf("%d : %d\n", ft_isalpha(126), isalpha(126));
-	printf("%d : %d\n", ft_isalpha(87), isalpha(87));
-} */
