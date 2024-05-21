4731eb46c85761c91659a02001005935fbce56be
Aurore Martinez <aumartin@bess-f1r3s6.clusters.42paris.fr>
2024-05-21 10:02:50 +0200

    memset compile et check ok

--- ft_memset.c
+++ ft_memset.c
@@ -6,27 +6,11 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:56:20 by aumartin          #+#    #+#             */
-/*   Updated: 2024/05/20 13:53:49 by aumartin         ###   ########.fr       */
+/*   Updated: 2024/05/21 10:02:25 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/* void	*ft_memset(void *b, int c, size_t len)
-{
-    unsigned char *ptr;
-
-    if (!b)
-		return (NULL);
-    ptr = (unsigned char *)b; // Convertir le pointeur b en un pointeur vers un caractère non signé
-    while (len > 0)
-    {
-        *ptr = (unsigned char)c; // Affecter la valeur c à l'octet pointé par ptr
-        ptr++; // Avancer le pointeur vers le prochain octet
-        len--; // Décrémenter le nombre d'octets restants
-    }
-    return b; // Retourner le pointeur vers le début de la mémoire modifiée
-} */
 
 void	*ft_memset(void *b, int c, size_t len)
 {
