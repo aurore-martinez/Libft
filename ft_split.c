9c57b63c9435ed5929d210e9c75bcdae07b10ed5
Aurore Martinez <aumartin@bess-f2r2s12.clusters.42paris.fr>
2024-05-23 09:55:55 +0200

    split compile et check ok

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,25 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:55:51 by aumartin          #+#    #+#             */
-/*   Updated: 2024/05/22 15:26:31 by aumartin         ###   ########.fr       */
+/*   Updated: 2024/05/23 09:55:06 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* void free_split(char **str)
+/* void free_split(char **str, int index)
 {
-	if (!*str)
-		return ;
+	int	i;
+
+	i = 0;
+	while (i < index)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
 }
- */
+*/
 
 int	count_strs(char *str, char c)
 {
@@ -26,7 +33,7 @@ int	count_strs(char *str, char c)
 
 	count = 0;
 	i = 0;
-	while (*str)
+	while (str[i])
 	{
 		if (str[i] != c && (str[i + 1] == c || str[i + 1] == '\0'))
 			count++;
@@ -47,13 +54,6 @@ char	*get_word(char *str, char c)
 	if (!word)
 		return (NULL);
 	ft_strlcpy(word, str, len_word + 1);
-/* 	i = 0;
-	while (i < len_word)
-	{
-		word[i] = str[i];
-		i++;
-	}
-	word[len_word] = '\0'; */
 	return (word);
 }
 
@@ -64,7 +64,7 @@ char	**ft_split(char *str, char c)
 	char	**strings;
 
 	count = count_strs(str, c);
-	strings = (char **)malloc(sizeof(char *) * (count + 1));
+	strings = (char **)ft_calloc(count + 1, sizeof(char *));
 	if (!strings)
 		return (NULL);
 	index = 0;
@@ -72,16 +72,17 @@ char	**ft_split(char *str, char c)
 	{
 		if (*str != c)
 		{
-		strings[index++] = get_word(str, c);
-			if (!strings[index++])
+		strings[index] = get_word(str, c);
+			if (!strings[index])
 			{
 				while (index--)
 					free(strings[index]);
 				free(strings);
 				return (NULL);
 			}
+			index++;
 			while (*str && *str != c)
-			str++;
+				str++;
 		}
 		else
 			str++;
@@ -90,7 +91,7 @@ char	**ft_split(char *str, char c)
 	return (strings);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 int main() {
 char **split = ft_split(",,,,coucou, salut,,,,azazaz", ',');
 if (split == NULL) {
@@ -111,5 +112,4 @@ i++;
 }
 free(split);
 return 0;
-}
-
+} */
