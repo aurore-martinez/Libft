d5a358b2839d9c356c9d99a8eba3553e10587b4a
Aurore Martinez <aumartin@bess-f2r2s12.clusters.42paris.fr>
2024-05-23 12:41:25 +0200

    striteri en cours + toutes les fonctions fd ok

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 14:31:22 by aumartin          #+#    #+#             */
-/*   Updated: 2024/05/23 12:01:06 by aumartin         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:13:13 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char *str, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
