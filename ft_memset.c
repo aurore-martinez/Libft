529337f76942b0785e6eb11bf760f29941bcca8d
Aurore Martinez <aumartin@bess-f2r3s14.clusters.42paris.fr>
2024-05-24 13:35:01 +0200

    memset retirer condition si retourrne null

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:56:20 by aumartin          #+#    #+#             */
-/*   Updated: 2024/05/22 14:13:58 by aumartin         ###   ########.fr       */
+/*   Updated: 2024/05/24 13:33:53 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{
